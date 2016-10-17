61921a57e8b1450a40cae78470c28d3317a3e47b
bill <bjh89210@163.com>
2016-10-17 19:25:56 +0800

    初始化工程

---
+++ LineChartsPersonalDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LineChartsPersonalDemo
+//
+//  Created by bill on 16/10/17.
+//  Copyright © 2016年 bill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LineChartsPersonalDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LineChartsPersonalDemo
+//
+//  Created by bill on 16/10/17.
+//  Copyright © 2016年 bill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
